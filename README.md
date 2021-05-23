507b8b88e9c5d26cb89490fa4d6c8731d182cfe3
Elsa Lebas <lebas.elsa@gmail.com>
2021-05-23 18:27:09 +0200

    setup for custom marker with image

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+Small rails app to practice GEOCODING using :
+ * gem 'geocoder'
+ * mapbox gl (mcustomed markers, pop-up on markers)
+
+
 Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,15 +9,24 @@ const buildMap = (mapElement) => {
 };
 
 const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(map);
-  });
-};
+markers.forEach((marker) => {
+  const popup = new mapboxgl.Popup().setHTML(marker.info_window);
 
+  // Create a HTML element for your custom marker
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('${marker.image_url}')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '25px';
+  element.style.height = '25px';
+
+    // Pass the element as an argument to the new marker
+    new mapboxgl.Marker(element)
+    .setLngLat([marker.lng, marker.lat])
+    .setPopup(popup)
+    .addTo(map);
+});
+};
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
