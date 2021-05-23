7c9694819369942c77fa492328866afb502cfd40
Elsa Lebas <lebas.elsa@gmail.com>
2021-05-23 14:09:15 +0200

    create partial for pop up window

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,10 +4,12 @@ class FlatsController < ApplicationController
   # GET /flats or /flats.json
   def index
     @flats = Flat.all
+
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { flat: flat })
       }
     end
   end
