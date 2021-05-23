55119c3db628de6d092543e3c8a6f1259cb4c678
Elsa Lebas <lebas.elsa@gmail.com>
2021-05-23 13:39:06 +0200

    js mapbox setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,6 +30,10 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 // Map int with Mapbox
+
+// CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
